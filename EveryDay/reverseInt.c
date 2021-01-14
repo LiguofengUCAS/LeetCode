14341affd4d25e5be2c06ff7028450cd3cfc28d9
LiguofengUCAS <liguofeng18@mails.ucas.ac.cn>
2021-01-14 23:34:26 +0800

    reverse integer

--- EveryDay/reverseInt.c
+++ EveryDay/reverseInt.c
@@ -4,7 +4,7 @@ int reverse(int x){
 
     while(x != 0)
     {   
-        if(tmp > 214748364 || tmp < -214748364)  //garantee -2^31 <= tmp <= 2^31 - 1
+        if(tmp > 214748364 || tmp < -214748364)  //to garantee -2^31 <= tmp <= 2^31 - 1
             return 0;  
 
         tmp *= 10;
