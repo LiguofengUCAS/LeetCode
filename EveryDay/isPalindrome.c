d7eba4001d55180a341e606360c088ce4c173d03
LiguofengUCAS <liguofeng18@mails.ucas.ac.cn>
2021-01-14 22:24:23 +0800

    elegent palindrome solution

--- EveryDay/isPalindrome.c
+++ EveryDay/isPalindrome.c
@@ -2,20 +2,14 @@ bool isPalindrome(int x){
     long reverse = 0;
     int y = x;
 
-    if(x < 0)
-        return false;
-    else
+    while(x != 0)
     {
-        while(x != 0)
-        {
-            reverse *= 10;
-            reverse += x % 10;
-            x = x / 10;
-        }
-        
-        if(y == reverse)
-            return true;
-        else  
-            return false;
+        reverse *= 10;
+        reverse += x % 10;
+        x = x / 10;
     }
+    
+    return reverse == y && y >= 0;
 }
+
+//This is the most elegent solution I have ever seen!
