56c9b1bec58bbe0c093e0e56c30ef934a62f9150
LiguofengUCAS <liguofeng18@mails.ucas.ac.cn>
2021-01-14 23:31:36 +0800

    myAtoi

---
+++ EveryDay/reverseInt.c
@@ -0,0 +1,16 @@
+int reverse(int x){
+    int tmp = 0;
+    int y = x;
+
+    while(x != 0)
+    {   
+        if(tmp > 214748364 || tmp < -214748364)  //garantee -2^31 <= tmp <= 2^31 - 1
+            return 0;  
+
+        tmp *= 10;
+        tmp += x % 10;
+        x /= 10;
+    }
+
+    return tmp;
+}
